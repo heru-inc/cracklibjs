4cf07ace300e4661a9588a361c51f9c55763a0cb
Dj Padzensky <djpadz@mac.com>
2025-05-27 22:20:29 -0700

    Added some enhancements to make it faster and stronger

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,25 @@
-import {
-  PasswordValidationError,
-  getWords,
-  includes,
-  isBasedOn,
-} from './util'
+import { PasswordValidationError, getWords, includes, isBasedOn } from "./util";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const cracklib = ({
-  dict = '/usr/share/dict/words',
-  minLength = 8,
-  loose = false,
-} = {}): (w?: string) => string | Error => {
-  const words = getWords(dict, loose)
-  return (word = '') => {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+const cracklib = ({ dict = "/usr/share/dict/words", minLength = 8, loose = false } = {}): ((
+  w?: string
+) => string | Error) => {
+  const words = getWords(dict, loose);
+  return (word = "") => {
     if (!word.length || /^\s+$/.test(word)) {
-      throw new PasswordValidationError('Password is empty or all whitespace')
+      throw new PasswordValidationError("Password is empty or all whitespace");
     }
     if (word.length < minLength) {
-      throw new PasswordValidationError('Password is too short')
+      throw new PasswordValidationError("Password is too short");
     }
     if (includes(word, words)) {
-      throw new PasswordValidationError('Password is too common')
+      throw new PasswordValidationError("Password is too common");
     }
     if (isBasedOn(word, words)) {
-      throw new PasswordValidationError('Password is too similar to a dictionary word')
+      throw new PasswordValidationError("Password is too similar to a dictionary word");
     }
-    return word
-  }
-}
+    return word;
+  };
+};
 
-export default cracklib
+export default cracklib;
--- src/util.ts
+++ src/util.ts
@@ -1,107 +1,64 @@
-import { createHash } from 'crypto'
-import { readFileSync, statSync } from 'fs'
-import { resolve } from 'path'
+import { createHash } from "crypto";
+import { readFileSync, statSync } from "fs";
+import { resolve } from "path";
 
 const isFile = (filePath: string): boolean => {
   try {
-    return statSync(filePath).isFile()
+    return statSync(filePath).isFile();
   } catch {
-    return false
+    return false;
   }
-}
-
-const uniq = <T>(a: T[]): T[] => [ ...new Set(a) ]
+};
 
 export class PasswordValidationError extends Error {
-  constructor (message: string) {
-    super(message)
-    this.name = 'PasswordValidationError'
+  constructor(message: string) {
+    super(message);
+    this.name = "PasswordValidationError";
   }
 }
 
-type Method = 'md5' | 'sha1' | 'sha256' | 'sha512'
+type Method = "md5" | "sha1" | "sha256" | "sha512";
 
-const hashString = (method: Method) => (s = ''): string =>
-  createHash(method).update(s).digest('hex')
+const hashString =
+  (method: Method) =>
+  (s = ""): string =>
+    createHash(method).update(s).digest("hex");
 
-const md5String = hashString('md5')
-const sha1String = hashString('sha1')
-const sha256String = hashString('sha256')
-const sha512String = hashString('sha512')
+const md5String = hashString("md5");
+const sha1String = hashString("sha1");
+const sha256String = hashString("sha256");
+const sha512String = hashString("sha512");
 
-const hashWord = (s: string = ''): string[] => [
-  md5String(s),
-  sha1String(s),
-  sha256String(s),
-  sha512String(s),
-]
+const hashWord = (s: string = ""): string[] => [md5String(s), sha1String(s), sha256String(s), sha512String(s)];
 
 const buildWordList = (dict: string): string[] =>
-  readFileSync(dict, 'utf8')
-    .split('\n')
-    .filter((a) => a)
+  readFileSync(dict, "utf8")
+    .split("\n")
+    .filter((a) => a);
 
-const reverseString = (s: string = ''): string => s.split('').reverse().join('')
+const reverseString = (s: string = ""): string => s.split("").reverse().join("");
 
-const buildDefaultWordList = (): string[] =>
-  buildWordList(resolve(__dirname, 'cracklib-small.txt'))
+const defaultWordList = (): string[] => buildWordList(resolve(__dirname, "cracklib-small.txt"));
 
-const trim = (s: string = ''): string => s.trim()
+const trim = (s: string = ""): string => s.trim();
 
-const normalize = (s: string = ''): string => trim(s.toLowerCase())
+const normalize = (s: string = ""): string => trim(s.toLowerCase());
 
-export const getWords = (dict: string, loose?: boolean): string[] => {
-  const words = isFile(dict) ? buildWordList(dict) : buildDefaultWordList()
-  const forward = ((words.length > 0) ? words : buildDefaultWordList()).map(
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    loose ? trim : normalize,
-  )
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  if (loose) {
-    return uniq(forward).sort()
-  }
-  const hashedWords = forward.map(hashWord)
+export const getWords = (dict: string | string[], loose: boolean = false): Set<string> => {
+  const words = Array.isArray(dict) ? dict : isFile(dict) ? buildWordList(dict) : [];
+  const forward = (words.length > 0 ? words : defaultWordList()).map(loose ? trim : normalize);
+  return new Set(loose ? forward : [...forward, ...forward.map(reverseString), ...forward.map(hashWord).flat()]);
+};
 
-  /* eslint-disable @typescript-eslint/require-array-sort-compare */
-  // @ts-expect-error this is correct actually
-  return uniq([
-    ...forward,
-    ...forward.map(reverseString),
-    ...hashedWords,
-  ]).sort()
-  /* eslint-enable @typescript-eslint/require-array-sort-compare */
-}
+export const includes = (el: string, xs: Set<string>): boolean => xs.has(el);
 
-// like a binary search
-export const includes = (el: string, xs: string[]): boolean => {
-  // i have no idea how efficient native includes actually is.
-  // probably a linear search though?
-  if (xs.length < 500) {
-    return xs.includes(el)
-  }
-
-  let minIndex = 0
-  let maxIndex = xs.length - 1
-  let currentIndex
-  let currentElement
-
-  while (minIndex <= maxIndex) {
-    currentIndex = Math.floor((minIndex + maxIndex) / 2)
-    currentElement = xs[currentIndex]
-
-    if (currentElement < el) {
-      minIndex = currentIndex + 1
-    } else if (currentElement > el) {
-      maxIndex = currentIndex - 1
-    } else {
-      return true
-    }
-  }
-
-  return false
-}
-
-export const isBasedOn = (el: string, xs: string[]): boolean => {
-  const replaced = el.toLowerCase().replace(/[\p{P}$+<=>^`|~]/gu, '')
-  return includes(replaced, xs)
-}
+export const isBasedOn = (el: string, xs: Set<string>): boolean => {
+  const replaced = el
+    .toLowerCase()
+    .replace("0", "o")
+    .replace("1", "l")
+    .replace("@", "a")
+    .replace("$", "s")
+    .replace(/[^a-z]/g, "");
+  return includes(replaced, xs);
+};
